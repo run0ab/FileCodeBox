8438829ea60aaf57ce69b70ef31d9a52147f46aa
run0ab <119866760+run0ab@users.noreply.github.com>
2024-03-30 05:53:57 +0000

    Update settings.py
    更新图床服务器域名

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ default_value = {
     'uploadSize': 1024 * 1024 * 1000,
     'uploadMinute': 1,
     'opacity': 0.9,
-    'background': 'https://dashboard.render.com/fcb/background.jpg',
+    'background': 'https://picsever.onrender.com/fcb/background.jpg',
     'uploadCount': 10,
     'errorMinute': 1,
     'errorCount': 1,
